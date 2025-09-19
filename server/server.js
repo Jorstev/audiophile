c93efa5dcef9e88e4bd0f3f256eeb99a5e85d794
Jorstev <71983628+Jorstev@users.noreply.github.com>
2025-09-18 19:52:37 -0600

    Fix(The wildcard * must have a name, matching the behavior of parameters :, use /*splat instead of /*)

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   const buildPath = path.join(__dirname, "client", "dist");
   app.use(express.static(buildPath));
 
-  app.get("/*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
