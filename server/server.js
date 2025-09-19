c7acc892d58e32769bd8c878d587a91583aa84f2
Jorstev <71983628+Jorstev@users.noreply.github.com>
2025-09-18 18:24:25 -0600

    Fix(app.get(/*))

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   const buildPath = path.join(__dirname, "client", "dist");
   app.use(express.static(buildPath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(buildPath, "index.html"));
   });
 }
